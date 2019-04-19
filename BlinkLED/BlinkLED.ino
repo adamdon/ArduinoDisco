329a6027a0812413016d1ee57ddcb556d0756dea
Adam Don <adamdon89@gmail.com>
2019-04-19 21:07:48 +0100

    simple pattern playback on LED (pin 13)  and button (pin 2)

--- BlinkLED/BlinkLED.ino
+++ BlinkLED/BlinkLED.ino
@@ -1,18 +1,44 @@
+const int buttonPin = 2; 
+const int ledPin =  13;     
+
+int buttonState = 0;       
 
 
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
+
 
 
 
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(100);                       
-  digitalWrite(LED_BUILTIN, LOW);    
-  delay(100);                      
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH)
+  {
+    runPattern(50);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  
+}
+
+
+static void runPattern(int intWaitTime)
+{
+    digitalWrite(ledPin, HIGH); 
+    delay(intWaitTime);                       
+    digitalWrite(ledPin, LOW);    
+    delay(intWaitTime); 
+    digitalWrite(ledPin, HIGH); 
+    delay(intWaitTime * 10);                       
+    digitalWrite(ledPin, LOW);    
+    delay(intWaitTime); 
 }
